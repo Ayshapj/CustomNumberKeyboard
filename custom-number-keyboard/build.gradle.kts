f2ab84447ef2a5982230b93bcb9bbb03bfa5567b
FZ00641 <aysha.pj@emsyne.com>
2026-01-09 10:36:16 +0530

    first commit

--- custom-number-keyboard/build.gradle.kts
+++ custom-number-keyboard/build.gradle.kts
@@ -1,15 +1,11 @@
-import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.maven.MavenPublication
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("maven-publish")
 }
 
 android {
     namespace = "com.emsyne.customkeyboard"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
@@ -18,9 +14,6 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-        }
-        debug {
             isMinifyEnabled = false
         }
     }
@@ -39,27 +32,4 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.10.0")
-}
-
-/* ---------------------------------------------------
-   MAVEN PUBLISH (SAFE & ANDROID-PROOF)
---------------------------------------------------- */
-configure<PublishingExtension> {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "com.emsyne"
-            artifactId = "customkeyboard"
-            version = "1.0.0"
-
-            // 👇 DIRECTLY PUBLISH THE AAR
-            artifact("$buildDir/outputs/aar/custom-number-keyboard-release.aar")
-        }
-    }
-
-    repositories {
-        maven {
-            name = "localRepo"
-            url = uri("${rootProject.projectDir}/local-repo")
-        }
-    }
 }
